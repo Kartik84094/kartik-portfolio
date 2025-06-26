acc9de86b6226a1afbb249fb24bcb02c1ab7954f
kartik kumar <kartikkumar84094@gmail.com>
2025-06-26 08:23:11 +0530

    remove width on side bar

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -4,7 +4,7 @@ import NavMenu from './NavMenu';
 
 export default function Sidebar() {
   return (
-    <aside className="w-64 bg-gray-800 p-4 flex flex-col items-center">
+    <aside className="bg-gray-800 p-4 flex flex-col items-center">
       <ProfileCard />
       <NavMenu />
       {/* <button className="mt-6 bg-green-500 px-4 py-2 rounded hover:bg-green-600">
